931a2dfb57478b2eb155573554e17777c93674b4
Moe Alkhatib <leonarther16th@gmail.com>
2014-05-01 12:56:36 -0400

    fixed users in the application controller so that it can save other fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+	def update_sanitized_params
+	  devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:first_name, :last_name,:agency_id, 
+	  															:agency_name, :email, :current_password, :password, :password_confirmation)}
+	end
+
   before_action :configure_devise_permitted_parameters if :device_controller?
 
   protected
