49c726fbf284e11ed7ef81968a337d667d33b77d
Moe Alkhatib <leonarther16th@gmail.com>
2014-05-04 01:13:11 -0400

    Minar layout fixes, created def_vars method in search_controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,7 @@
 class SearchesController < ApplicationController
   before_action :set_search, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :def_vars, only: [:new, :edit, :create, :update, :seat]
   # GET /searches
   # GET /searches.json
   def index
@@ -17,14 +18,12 @@ class SearchesController < ApplicationController
   def new
     @search = Search.new
     @search.user_id = current_user.id
-    @courses = Course.all
-    @cities = City.all
+    
   end
 
   # GET /searches/1/edit
   def edit
-    @courses = Course.all
-    @cities = City.all
+    
   end
 
   def find
@@ -35,7 +34,7 @@ class SearchesController < ApplicationController
   # POST /searches.json
   def create
     @search = Search.new(search_params)
-    @cities = City.all
+   
 
     respond_to do |format|
       if @search.save
@@ -51,7 +50,7 @@ class SearchesController < ApplicationController
   # PATCH/PUT /searches/1
   # PATCH/PUT /searches/1.json
   def update
-    @cities = City.all
+   
     respond_to do |format|
       if @search.update(search_params)
         format.html { redirect_to @search, notice: 'Search was successfully updated.' }
@@ -77,14 +76,18 @@ class SearchesController < ApplicationController
     @seat = Seat.all
     @search = Search.new
     @search.user_id = current_user.id
-    @courses = Course.all
-    @cities = City.all
+    
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_search
       @search = Search.find(params[:id])
+    end
+
+    def def_vars
+      @courses = Course.all
+      @cities = City.all
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
