4f5c31feb807f232c7cc212ab3a2e57741d10738
Moe Alkhatib <leonarther16th@gmail.com>
2014-07-10 01:45:08 -0400

    Added some JS layout enhancment

---
+++ app/assets/javascripts/beauty.js
@@ -0,0 +1,16 @@
+
+$(".book-now").toggleClass("hider");
+
+$(".seat-well").hover(function(){
+	$(this).addClass("focus");
+	
+	$(this).find(".book-now").toggleClass("hider");
+	
+
+	
+}, function(){
+	$(this).removeClass("focus");
+	$(this).find(".book-now").toggleClass("hider");
+	
+});
+
