78fd3af9a139f3a5901c7fec83812fa0b5f369a9
Moe Alkhatib <leonarther16th@gmail.com>
2014-09-03 01:53:39 -0400

    enabled search bound by num of weeks

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -79,7 +79,7 @@ class SearchesController < ApplicationController
     @search = Search.new
     @search.user_id = current_user.id
     @last_search = Search.find(params[:id])
-    @offers = Offer.where('? between start_date and end_date', @last_search.start_date)
+    @offers = Offer.where('? between start_date and end_date and ? between min_num_weeks and max_num_weeks', @last_search.start_date, @last_search.num_weeks)
     @s = 'toronto-drowing.jpg'
     user_settings = current_user.user_setting
     @increase_by = user_settings.setting['increase_prices_by'].to_f
