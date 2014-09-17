3deff07b0adac37378551cfbf4bca15ba3e09f50
Moe Alkhatib <leonarther16th@gmail.com>
2014-09-16 23:57:50 -0400

    update order page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+	def labelTag(orderState)
+		if orderState == 'pending'
+			['label-warning', 'oi-clock']
+		elsif orderState == 'reserved'
+			['label-success', 'oi-check']
+		end
+			
+	end
 end
