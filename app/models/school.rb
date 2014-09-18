df9d4cab43ab4ff0922733a7ac2f475700f99a2c
Moe Alkhatib <leonarther16th@gmail.com>
2014-09-17 21:25:11 -0400

    added really stars to searches

--- app/models/school.rb
+++ app/models/school.rb
@@ -7,4 +7,8 @@ class School < ActiveRecord::Base
 	has_many :orders, :through => :offers
 
 	letsrate_rateable "quality", "price", "learning"
+
+	def avg_rate
+		q = Rate.where('rateable_type = ? and rateable_id = ?' ,'School', self.id).average(:stars).to_i
+	end
 end
