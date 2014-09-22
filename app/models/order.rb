c4462ab3879f15b449aa1406251dd559001004a4
Moe Alkhatib <leonarther16th@gmail.com>
2014-09-22 12:04:08 -0400

    removed edit and remove offer, updated dependancy for order after it is deleted

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
 	belongs_to :offer
 	belongs_to :user
-	has_one :school, :through => :offer
-	has_one :reservation
-	has_one :student
+	has_one :school, :through => :offer, :dependent => :delete
+	has_one :reservation, :dependent => :delete
+	has_one :student, :dependent => :delete
 end
