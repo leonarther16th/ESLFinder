ec60cdbeb283f3e639d2483bc17aded9d4ba8f53
Moe Alkhatib <leonarther16th@gmail.com>
2014-09-22 10:17:43 -0400

    fixed validation for reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -30,10 +30,11 @@ class ReservationsController < ApplicationController
 
     respond_to do |format|
       if @reservation.save
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
+        format.html { redirect_to @order, notice: 'Reservation was successfully created.' }
         format.json { render action: 'show', status: :created, location: @reservation }
       else
-        format.html { render action: 'new' }
+        @order = Order.find_by_id(@reservation.order_id)
+        format.html { render action: 'new'}
         format.json { render json: @reservation.errors, status: :unprocessable_entity }
       end
     end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ActiveRecord::Base
+	validates :card_type, :card_name, :card_number, :expiry_year, :expiry_month, :cvc, presence: true
 	belongs_to :order
 	belongs_to :user
+
 end
