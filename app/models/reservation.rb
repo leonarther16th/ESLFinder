a401009d9555e8f0244514845fceecad199ba6e0
Moe Katib <MoeKatib@Moes-iMac.local>
2014-11-03 15:22:51 -0500

    Fixed validation of reservation, and user setting

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,24 @@
 class Reservation < ActiveRecord::Base
-	validates :card_type, :card_name, :card_number, :expiry_year, :expiry_month, :cvc, presence: true
-	validates :card_number, :numericality => {:only_integer => true}, length: { minimum: 16, maximum: 16, too_long: "Please enter a valid credit card number", too_short: "Please enter a valid credit card number" }
-	validates :cvc, :numericality => {:only_integer => true}, length: { minimum: 3, maximum: 3, too_long: "Please enter a valid CVC number", too_short: "Please enter a valid credit card number" }
-	validates_format_of :card_name, :with => /^[a-zA-Z]+$/ , :multiline => true
+	
+	validates :card_type, :card_name, :card_number, :expiry_year, :expiry_month, :cvc, presence: { message: "This field is required, please!"}
+	validates :card_number, :numericality => {:only_integer => true, message: "Only numbers, please!"}, length: { minimum: 16, maximum: 16, message: "Please enter a valid credit card number"}
+	validates :card_name, length: { minimum: 4, maximum: 25, message: "Please enter a valid name"}
+	validates :cvc, :numericality => {:only_integer => true, message: "Only numbers, please!"}, length: { minimum: 3, maximum: 3, message: "Please enter a valid CVC number" }
+	validates_format_of :card_name, :with => /^[a-zA-Z]+$/ , :multiline => true, message: "Please type in a correct name!"
+	validate :experation_date_cannot_be_in_the_past
+	
 	belongs_to :order
 	belongs_to :user
+
+
+	def experation_date_cannot_be_in_the_past
+		if self.expiry_month.present? and self.expiry_year.present?
+			d = Date.new(self.expiry_year.to_i, self.expiry_month.to_i, 1)
+			if d < Date.today
+				errors.add(:expiry_month, "Please select a valid date!")
+				errors.add(:expiry_year, "Please select a valid date!")
+			end
+		end
+	end
 
 end
