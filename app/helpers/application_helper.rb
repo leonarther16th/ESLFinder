b3d869cb4ecb04d4cf5c96c9c731655b5cae08cb
Moe Katib <MoeKatib@Moes-iMac.local>
2014-11-07 11:41:24 -0500

    updated the agents setting area || fixed footer text bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 	def footer
 		@footer = 	{
-			agency: ["eslBooking is an easy way for you to increase your revenue without increasing your costs",
+			school: ["eslBooking is an easy way for you to increase your revenue without increasing your costs",
 					"eslBooking gives you the option of offering aggressive promotions without harming your brand ",
 					"eslBooking ensures that agents don’t take your low pricing for granted as it is offered through a third party site",
 					"eslBooking allows you to promote your prices without extra costs or effort by your Marketing team",
 					"eslBooking is online and updated regularly so you can enjoy 24/7 access"],
-			school: ["eslBooking is the first of its kind in the ESL industry, so you can be a part of history",
+			agency: ["eslBooking is the first of its kind in the ESL industry, so you can be a part of history",
 					"eslBooking is the perfect place to find the best deals and benefit from the lowest discounted prices",
 					"eslBooking helps you sell the empty seats in your partners classrooms",
 					"eslBooking is online and updated regularly so you can enjoy 24/7 access"],
