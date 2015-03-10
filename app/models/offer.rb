ce685336da10092fa59271df4aadf7cbce4c21ec
Moe Katib <Moe.Katib@gmail.com>
2015-03-10 00:46:40 -0400

    small bug fix

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -10,7 +10,7 @@ class Offer < ActiveRecord::Base
 
 	def can_book(search)
 		range = (self.start_date..self.end_date)
-		if range.include?(search[start_date]) and search[num_weeks].between?(this.min_num_weeks, this.max_num_weeks)
+		if range.include?(search[start_date]) and search[num_weeks].between?(self.min_num_weeks, self.max_num_weeks)
 			return -1
 		else
 			return 1
